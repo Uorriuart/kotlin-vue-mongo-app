46815cf2c50b520b5d00a969668a7d9298f5bd9d
Uorri <uorriart@yandex.ru>
2021-02-22 22:42:10 +0300

    Add a basic Kotlin project structure and a Gradle build spec.

---
+++ blog-api/src/test/kotlin/com/uorriuart/blogapi/BlogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uorriuart.blogapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
